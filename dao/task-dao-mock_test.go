c103cb05f4320e4c84d8daa46ec3222e9bceca37
Olivier FUXET <olivier.fuxet@gmail.com>
2017-03-21 17:39:51 +0100

    Add TODOs for dao tests

--- dao/task-dao-mock_test.go
+++ dao/task-dao-mock_test.go
@@ -1,61 +1,13 @@
-package dao_test
+package dao_test //Don't change this! Create a new test file instead.
 
 import (
-	"github.com/Sfeir/golang-200/dao"
-	"github.com/Sfeir/golang-200/model"
-	"github.com/satori/go.uuid"
 	"testing"
-	"time"
 )
 
 func TestDAOMock(t *testing.T) {
 
-	daoMock, err := dao.GetTaskDAO("", dao.DAOMock)
-	if err != nil {
-		t.Error(err)
-	}
-
-	toSave := model.Task{
-		ID:           uuid.NewV4().String(),
-		Title:        "Use Go",
-		Description:  "Let's use the Go programming language in a real project.",
-		Status:       model.StatusTodo,
-		Priority:     model.PriorityMedium,
-		CreationDate: time.Date(2017, 02, 01, 0, 0, 0, 0, time.UTC),
-		DueDate:      time.Date(2017, 02, 02, 0, 0, 0, 0, time.UTC),
-	}
-
-	err = daoMock.Save(&toSave)
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Log("initial task saved", toSave)
-
-	tasks, err := daoMock.GetAll(dao.NoPaging, dao.NoPaging)
-	if err != nil {
-		t.Error(err)
-	}
-	if len(tasks) != 2 {
-		t.Errorf("Expected 2 tasks, got %d", len(tasks))
-	}
-
-	oneTask, err := daoMock.GetByID(toSave.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	if toSave != *oneTask {
-		t.Error("Got wrong task by ID")
-	}
-
-	err = daoMock.Delete(oneTask.ID)
-	if err != nil {
-		t.Error(err)
-	}
-
-	oneTask, err = daoMock.GetByID(oneTask.ID)
-	if err == nil {
-		t.Error("Task should have been deleted", oneTask)
-	}
+	//TODO: implement this to test TaskDAOMock. Look at task-dao-mongo_test.go to see how to do.
 
 }
+
+//TODO: implement a test to test dao.getBy method. You may have to create another ****_test.go file...
